fdcd8e3bf89f45cbf1521f571715209162aea1c9
Tbxhs <desert@desert-pc.(none)>
2013-01-19 00:57:52 +0800

    新建程序

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bqey::Application.config.secret_token = '8ec1a9556f027a5c721fe3cba345ecdda64ba29e409580df00b11a0092bc6e5fb53f7c338ad9f8129591d8b73fda4d035518f5f6395e8ee0f4007b51d746b7e6'
